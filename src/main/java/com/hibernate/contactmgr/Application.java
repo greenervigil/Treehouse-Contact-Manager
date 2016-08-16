662cac214d389c2c997807da604c78837b73c7d9
Daniel Vigil <daniel.julian.vigil@gmail.com>
2016-08-16 09:41:05 -0400

    added fetchAllContact method and updated config to Update instead of create

--- src/main/java/com/hibernate/contactmgr/Application.java
+++ src/main/java/com/hibernate/contactmgr/Application.java
@@ -1,11 +1,14 @@
 package com.hibernate.contactmgr;
 
 import com.hibernate.contactmgr.model.Contact;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.service.ServiceRegistry;
+
+import java.util.List;
 
 
 public class Application {
@@ -24,6 +27,33 @@ public class Application {
                 .withPhone(5555558898L)
                 .build();
 
+        save(contact);
+
+        //Display list of contacts
+        for(Contact c : fetchAllContacts()) {
+            System.out.println(c);
+        }
+        //java 8 solution
+        //fetchAllContacts().stream().forEach(System.out::println);
+    }
+
+
+    private static List<Contact> fetchAllContacts() {
+        //Open session
+        Session session = sessionFactory.openSession();
+
+        //Create Criteria and get a list of them
+        Criteria criteria = session.createCriteria(Contact.class);
+
+        List<Contact> contacts = criteria.list();
+
+
+        //close session
+        session.close();
+        return contacts;
+    }
+
+    private static void save(Contact contact) {
         //open session
         Session session = sessionFactory.openSession();
         //begin transaction
